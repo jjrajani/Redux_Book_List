7c2ac9ea3725da6d175fe60b7c6f6e4ff1ad0251
humdrum <jjrajani@gmail.com>
2017-08-10 23:49:46 -0400

    setup book reducer and book-list container

---
+++ src/containers/index.js
@@ -0,0 +1,10 @@
+/*
+  A container component (aka smart component) is a component with a direct relationship
+  with the redux application state.
+*/
+
+import BookList from './book-list';
+
+export {
+  BookList,
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,25 @@
+/*
+A reducer is a function that returns
+a piece of the application state.
+*/
+
+/*
+Many pieces of application state,
+so many reduces.
+*/
+
+/*
+For this example we will have a reducer
+for returning a list of users as well as
+a reducer for returning a currently selected user
+*/
+
+import BooksReducer from './reducer_books';
+
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer,
 });
 
 export default rootReducer;
